e5dd1bd8be38bb2a2310f3f7044b3c5c5d3de4e0
shajith10 <dummymail071045@gmail.com>
2023-12-23 13:03:31 +0530

    appchange commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Routes,Route,Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import SiginUp from './COMPONENT/SiginUp';
 import Login from './COMPONENT/Login';
 import FoodFront from './COMPONENT/FoodFront';
@@ -9,13 +9,13 @@ function App() {
 
   return (
     <div>
-      <Routes basename="/FoodApp">
+      <Router basename="/FoodApp">
          <Switch >
           <Route path='/' element={<SiginUp />}></Route>
           <Route path='login' element={<Login />}></Route>
           <Route path='foodfront' element={<FoodFront />}></Route>
           </Switch>
-      </Routes>
+      </Router>
     
       
     </div>
